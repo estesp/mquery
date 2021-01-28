d48952d9a4dda449dc99f9653204d7aaa18ec760
Phil Estes <estesp@gmail.com>
2021-01-27 23:43:07 -0500

    Add lambda function
    Signed-off-by: Phil Estes <estesp@gmail.com>

---
+++ function/go.mod
@@ -0,0 +1,11 @@
+module github.com/estesp/mquery/function
+
+go 1.15
+
+require (
+	github.com/aws/aws-lambda-go v1.21.0
+	github.com/aws/aws-sdk-go v1.36.17
+	github.com/estesp/manifest-tool v1.0.4-0.20201218012658-2d360eeba276
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.0.1
+)
---
+++ function/inspect.go
@@ -0,0 +1,219 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+
+	"github.com/estesp/manifest-tool/pkg/registry"
+	"github.com/estesp/manifest-tool/pkg/store"
+	"github.com/estesp/manifest-tool/pkg/types"
+	"github.com/estesp/manifest-tool/pkg/util"
+
+	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
+)
+
+const (
+	cacheTimeout = time.Hour
+)
+
+var (
+	tableName  = "imagecache"
+	dynaClient dynamodbiface.DynamoDBAPI
+)
+
+type ErrorBody struct {
+	ErrorMsg *string `json:"error,omitempty"`
+}
+
+type Image struct {
+	CacheTS   int64              `json:"cachets"`
+	IsList    bool               `json:"islist"`
+	ImageName string             `json:"imagename"`
+	Digest    string             `json:"digest"`
+	ArchList  []ocispec.Platform `json:"archlist"`
+}
+
+func main() {
+	region := os.Getenv("AWS_REGION")
+	awsSession, err := session.NewSession(&aws.Config{
+		Region: aws.String(region)},
+	)
+	if err != nil {
+		return
+	}
+	dynaClient = dynamodb.New(awsSession)
+	lambda.Start(handleRequest)
+}
+
+func inspectImage(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	imageName := req.QueryStringParameters["image"]
+	if len(imageName) == 0 {
+		return apiResponse(http.StatusBadRequest, ErrorBody{aws.String("No image name provided")})
+	}
+	// check cache
+	if image, err := checkCache(imageName); err == nil {
+		return apiResponse(http.StatusOK, image)
+	}
+	// inspect image
+	image, err := queryRegistry(imageName)
+	if err != nil {
+		return apiResponse(http.StatusBadRequest, ErrorBody{aws.String(fmt.Sprintf("Error querying image: %s", err))})
+	}
+	if err = cacheImage(image); err != nil {
+		log.Printf("WARN: unable to cache image: %v", err)
+	}
+
+	return apiResponse(http.StatusOK, image)
+}
+
+func handleRequest(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	switch req.HTTPMethod {
+	case "GET":
+		return inspectImage(req)
+	}
+	return apiResponse(http.StatusMethodNotAllowed, "method not allowed")
+}
+
+func apiResponse(status int, body interface{}) (*events.APIGatewayProxyResponse, error) {
+	resp := events.APIGatewayProxyResponse{Headers: map[string]string{"Content-Type": "application/json"}}
+	resp.StatusCode = status
+
+	stringBody, _ := json.Marshal(body)
+	resp.Body = string(stringBody)
+	return &resp, nil
+}
+
+func checkCache(imageName string) (*Image, error) {
+	input := &dynamodb.GetItemInput{
+		Key: map[string]*dynamodb.AttributeValue{
+			"imagename": {
+				S: aws.String(imageName),
+			},
+		},
+		TableName: aws.String(tableName),
+	}
+
+	result, err := dynaClient.GetItem(input)
+	if err != nil {
+		return nil, errors.New("failed to find image")
+	}
+
+	item := new(Image)
+	err = dynamodbattribute.UnmarshalMap(result.Item, item)
+	if err != nil {
+		return nil, errors.New("failed to unmarshal image cached details")
+	}
+	cacheTime := time.Unix(item.CacheTS, 0)
+	if cacheTime.Add(cacheTimeout).Before(time.Now()) {
+		// invalidate
+		deleteCache(imageName)
+		return nil, errors.New("Cache expired image " + imageName)
+	}
+	return item, nil
+}
+
+func deleteCache(imageName string) {
+	input := &dynamodb.DeleteItemInput{
+		Key: map[string]*dynamodb.AttributeValue{
+			"imagename": {
+				S: aws.String(imageName),
+			},
+		},
+		TableName: aws.String(tableName),
+	}
+
+	dynaClient.DeleteItem(input)
+}
+
+func cacheImage(image *Image) error {
+	av, err := dynamodbattribute.MarshalMap(image)
+	if err != nil {
+		return errors.New("could not marshal image data")
+	}
+
+	input := &dynamodb.PutItemInput{
+		Item:      av,
+		TableName: aws.String(tableName),
+	}
+
+	_, err = dynaClient.PutItem(input)
+	if err != nil {
+		return errors.New("could not write to dynamoDB")
+	}
+	return nil
+}
+
+func queryRegistry(name string) (*Image, error) {
+	var image *Image
+	imageRef, err := util.ParseName(name)
+	if err != nil {
+		return nil, err
+	}
+
+	memoryStore := store.NewMemoryStore()
+	resolver := util.NewResolver("", "", false, false, "")
+
+	descriptor, err := registry.FetchDescriptor(resolver, memoryStore, imageRef)
+	if err != nil {
+		return nil, err
+	}
+
+	_, db, _ := memoryStore.Get(descriptor)
+	switch descriptor.MediaType {
+	case ocispec.MediaTypeImageIndex, types.MediaTypeDockerSchema2ManifestList:
+		// this is a multi-platform image descriptor; marshal to Index type
+		var idx ocispec.Index
+		if err := json.Unmarshal(db, &idx); err != nil {
+			return nil, err
+		}
+		image = generateImage(name, memoryStore, descriptor, idx, ocispec.Image{})
+	case ocispec.MediaTypeImageManifest, types.MediaTypeDockerSchema2Manifest:
+		var man ocispec.Manifest
+		if err := json.Unmarshal(db, &man); err != nil {
+			return nil, err
+		}
+		_, cb, _ := memoryStore.Get(man.Config)
+		var conf ocispec.Image
+		if err := json.Unmarshal(cb, &conf); err != nil {
+			return nil, err
+		}
+		image = generateImage(name, memoryStore, descriptor, ocispec.Index{}, conf)
+		return image, nil
+	default:
+		return nil, errors.New("Unknown descriptor type: " + descriptor.MediaType)
+	}
+	return image, nil
+}
+
+func generateImage(name string, cs *store.MemoryStore, desc ocispec.Descriptor, index ocispec.Index, imgConfig ocispec.Image) *Image {
+	image := new(Image)
+	image.Digest = desc.Digest.String()
+	image.ImageName = name
+	image.CacheTS = time.Now().Unix()
+	switch desc.MediaType {
+	case ocispec.MediaTypeImageIndex, types.MediaTypeDockerSchema2ManifestList:
+		image.IsList = true
+		for _, img := range index.Manifests {
+			image.ArchList = append(image.ArchList, *img.Platform)
+		}
+	default:
+		image.ArchList = []ocispec.Platform{{
+			OS:           imgConfig.OS,
+			Architecture: imgConfig.Architecture,
+		}}
+	}
+	return image
+}
