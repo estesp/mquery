9a3aed32989bd5392b4230c463d8f5b372444b8d
Phil Estes <estesp@gmail.com>
2023-03-06 12:28:17 -0500

    Update vendors for new manifest-tool version
    Signed-off-by: Phil Estes <estesp@gmail.com>

--- function/vendor/github.com/estesp/manifest-tool/v2/pkg/util/resolver.go
+++ function/vendor/github.com/estesp/manifest-tool/v2/pkg/util/resolver.go
@@ -1,17 +1,20 @@
 package util
 
 import (
-	"context"
 	"crypto/tls"
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/containerd/containerd/remotes"
 	"github.com/containerd/containerd/remotes/docker"
-	auth "oras.land/oras-go/pkg/auth/docker"
+	"github.com/docker/cli/cli/config"
+	"github.com/docker/cli/cli/config/configfile"
+	"github.com/docker/cli/cli/config/credentials"
 	"github.com/sirupsen/logrus"
 )
 
-func NewResolver(username, password string, insecure, plainHTTP bool, configs ...string) remotes.Resolver {
+func NewResolver(username, password string, insecure, plainHTTP bool, dockerConfigPath string) remotes.Resolver {
 
 	opts := docker.ResolverOptions{
 		PlainHTTP: plainHTTP,
@@ -32,14 +35,58 @@ func NewResolver(username, password string, insecure, plainHTTP bool, configs ..
 		}
 		return docker.NewResolver(opts)
 	}
-	cli, err := auth.NewClient(configs...)
-	if err != nil {
-		logrus.Warnf("Error loading auth file: %v", err)
+	var (
+		err error
+		cfg *configfile.ConfigFile
+	)
+	if dockerConfigPath == "" || dockerConfigPath == config.Dir() {
+		cfg, err = config.Load(config.Dir())
+		if err != nil {
+			// handle error
+			logrus.Errorf("unable to load default Docker auth config: %v", err)
+		}
+	} else {
+		cfg = configfile.New(dockerConfigPath)
+		if _, err := os.Stat(dockerConfigPath); err == nil {
+			file, err := os.Open(dockerConfigPath)
+			if err != nil {
+				logrus.Errorf("Can't load docker config file %s: %v", dockerConfigPath, err)
+				// fall back to resolver with no config
+				return docker.NewResolver(opts)
+			}
+			defer file.Close()
+			if err := cfg.LoadFromReader(file); err != nil {
+				logrus.Errorf("Can't read and parse docker config file %s: %v", dockerConfigPath, err)
+				return docker.NewResolver(opts)
+			}
+		} else if !os.IsNotExist(err) {
+			logrus.Errorf("Unable to open docker config file %s: %v", dockerConfigPath, err)
+			return docker.NewResolver(opts)
+		}
 	}
-	resolver, err := cli.Resolver(context.Background(), client, plainHTTP)
-	if err != nil {
-		logrus.Warnf("Error loading resolver: %v", err)
-		resolver = docker.NewResolver(opts)
+	if !cfg.ContainsAuth() {
+		cfg.CredentialsStore = credentials.DetectDefaultStore(cfg.CredentialsStore)
 	}
-	return resolver
+	// support cred helpers
+	opts.Credentials = func(hostName string) (string, string, error) {
+		hostname := resolveHostname(hostName)
+		auth, err := cfg.GetAuthConfig(hostname)
+		if err != nil {
+			return "", "", err
+		}
+		if auth.IdentityToken != "" {
+			return "", auth.IdentityToken, nil
+		}
+		return auth.Username, auth.Password, nil
+	}
+	return docker.NewResolver(opts)
+}
+
+// resolveHostname resolves Docker specific hostnames
+func resolveHostname(hostname string) string {
+	if strings.HasSuffix(hostname, "docker.io") {
+		// Docker's `config.json` uses index.docker.io as the reference
+		return LegacyDefaultHostname
+	}
+	return hostname
 }
